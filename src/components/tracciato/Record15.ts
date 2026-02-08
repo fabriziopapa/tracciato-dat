aade888f39b4290728ea271562529128a091f029
fabriziopapa <fabrizio.papa@uniparthenope.it>
2026-02-08 22:45:32 +0100

    Aggiunto Tracciato 00

--- src/components/tracciato/Record15.ts
+++ src/components/tracciato/Record15.ts
@@ -3,43 +3,39 @@ import { Testata } from './Testata';
 import { Field } from './Field';
 
 export class Record15 {
-    testata: Testata;
     fields: Field[] = [];
 
     constructor(testata: Testata) {
-        // Inizializzazione dei primi 9 campi comuni (riutilizzo della classe Testata)
-        this.testata = testata;
+        // Aggiungi i primi 9 campi del record 00 dalla testata
+        this.fields.push(...testata.fields);
 
-        // Aggiungi i campi specifici per il tipo record 15
-        this.fields.push(new Field('2022', 'numero', 4));         // ANNO
-        this.fields.push(new Field('F123456789012345', 'testo', 16));  // COD_FISC_SOSTITUTO
-        this.fields.push(new Field('01', 'numero', 2));           // PROGRC
-        this.fields.push(new Field('A123456789012345', 'testo', 16));  // COD_FISCO
-        this.fields.push(new Field('D', 'testo', 1));             // SOSTITUTO_TIPO_COMUNICAZIONE
-        this.fields.push(new Field('Sostituto comunicazione esempio', 'testo', 60)); // DESCR_SOSTITUTO
-        this.fields.push(new Field(new Date('2021-01-01'), 'data', 10)); // DATA_RICEZIONE
-        this.fields.push(new Field('X', 'testo', 1));             // QUADRO
-        this.fields.push(new Field('01', 'numero', 2));           // PROGR_CONGUAGLIO
-        this.fields.push(new Field('123456789012', 'testo', 12)); // CODICE_INPDAP_AMMINISTRAZIONE
-        this.fields.push(new Field(new Date('2021-12-31'), 'data', 10)); // DATA_CESSAZIONE
-        this.fields.push(new Field('RM', 'testo', 2));            // PROVINCIA_LAVORO
-        this.fields.push(new Field(new Date('2021-01-01'), 'data', 10)); // DATA_IND
-        this.fields.push(new Field(new Date('2021-12-31'), 'data', 10)); // DATA_FIND
-        this.fields.push(new Field('02', 'testo', 2));            // TIPO_IMPIEGO
-        this.fields.push(new Field('01', 'testo', 2));            // TIPO_SERVIZIO
-        this.fields.push(new Field('1', 'testo', 1));             // F_CONTRIBUTI_INPDAP
-        this.fields.push(new Field(new Date('2021-01-01'), 'data', 10)); // DATA_INSD
-        this.fields.push(new Field(new Date('2021-01-01'), 'data', 10)); // DATA_MOD
-        this.fields.push(new Field('Operator01', 'testo', 10));   // OPERATORE
-        this.fields.push(new Field('Capitolo01', 'testo', 6));    // CAPITOLO
-        this.fields.push(new Field('12', 'numero', 12));          // F_MESI_ESTRAC
-        this.fields.push(new Field('06', 'numero', 2));           // MESE_RIF
-        this.fields.push(new Field('01', 'numero', 2));           // CASSA_PENS
-        this.fields.push(new Field('T', 'testo', 1));             // TEMPO
+        // Aggiungi i campi specifici per il record 15
+        this.fields.push(new Field('70003', 'testo', 5));          // Codice Ateneo
+        this.fields.push(new Field('15', 'testo', 2));             // Tipo Record
+        this.fields.push(new Field('Sostituto', 'testo', 60));    // Descrizione Sostituto
+        this.fields.push(new Field('2022-11-01', 'data', 10));    // Data Ricezione
+        this.fields.push(new Field('Q', 'testo', 1));             // Quadro
+        this.fields.push(new Field('01', 'testo', 2));            // Progresso Conguaglio
+        this.fields.push(new Field('AB123', 'testo', 12));        // Codice INPDAP
+        this.fields.push(new Field('2022-12-31', 'data', 10));    // Data Cessazione
+        this.fields.push(new Field('NA', 'testo', 2));            // Provincia Lavoro
+        this.fields.push(new Field('2022-01-01', 'data', 10));    // Data Inizio
+        this.fields.push(new Field('2022-12-31', 'data', 10));    // Data Fine
+        this.fields.push(new Field('1', 'testo', 2));             // Tipo Impiego
+        this.fields.push(new Field('01', 'testo', 2));            // Tipo Servizio
+        this.fields.push(new Field('1', 'testo', 1));             // F Contributi INPDAP
+        this.fields.push(new Field('2022-11-01', 'data', 10));    // Data Inizio (INS)
+        this.fields.push(new Field('2022-12-01', 'data', 10));    // Data Modifica
+        this.fields.push(new Field('Operator1', 'testo', 10));   // Operatore
+        this.fields.push(new Field('123456', 'testo', 6));        // Capitolo
+        this.fields.push(new Field('12', 'testo', 12));           // F Mesi Estrac
+        this.fields.push(new Field('05', 'testo', 2));            // Mese Rif
+        this.fields.push(new Field('P', 'testo', 2));             // Cassa Pensione
+        this.fields.push(new Field('Y', 'testo', 1));             // Tempo
     }
 
     toString(): string {
-        // Combina la testata con i campi specifici del record 15
-        return this.testata.toString() + this.fields.map(field => field.toString()).join('@') + '@';
+        return this.fields.map(field => field.toString()).join('@') + '@';
     }
 }
+
--- src/components/tracciato/Testata.ts
+++ src/components/tracciato/Testata.ts
@@ -5,16 +5,16 @@ export class Testata {
     fields: Field[] = [];
 
     constructor() {
-        // Inizializzazione dei primi 9 campi comuni a tutti i record (Tipo 00)
+        // Inizializzazione dei primi 9 campi per il record 00 (testata)
         this.fields.push(new Field('000000', 'numero', 6));     // ProgrGen
         this.fields.push(new Field('I', 'testo', 1));           // TipoOperazione
+        this.fields.push(new Field('', 'testo', 1));            // SeparatorChr
         this.fields.push(new Field('', 'testo', 1));            // SeparatorChr
         this.fields.push(new Field(15, 'numero', 2));           // TotCampi
         this.fields.push(new Field('00', 'testo', 2));          // TipoRecord
         this.fields.push(new Field('000000', 'numero', 6));     // ProgrTipoRec
         this.fields.push(new Field('0', 'testo', 1));           // Comparto
         this.fields.push(new Field('0000', 'testo', 4));        // RuoloVariabile
-        this.fields.push(new Field('000000', 'testo', 6));      // Matricola
     }
 
     toString(): string {
