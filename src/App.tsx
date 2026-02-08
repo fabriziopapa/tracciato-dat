a60a54b0ba78e26f50a566427ce97bbf5de51b4e
fabriziopapa <fabrizio.papa@uniparthenope.it>
2026-02-08 22:34:08 +0100

    Aggiungi prime classi e app temp react attivo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 
-import React from 'react';
+import React from 'react';  // Importazione di React
 import { Testata } from './components/tracciato/Testata';
 import { Record15 } from './components/tracciato/Record15';
 
@@ -14,13 +14,18 @@ const App = () => {
   // Genera il tracciato per il record 15
   const tracciato = record15.toString();
 
+  // Utilizziamo React esplicitamente nel JSX, per esempio usando un componente div
   return (
     <div>
       <h1>Tracciato Record 15</h1>
       <pre>{tracciato}</pre>
+
+      {/* Un esempio di utilizzo esplicito di React.createElement */}
+      {React.createElement('footer', null, 'Generato tramite React')}
     </div>
   );
 };
 
 export default App;
+
 
